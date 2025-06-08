876639d44bb8c4bc7387838dcdebc1c0c0a3f7d3
Space Cats <kittieswillkillyouall@gmail.com>
2025-06-08 13:27:28 -0400

    vector2D

---
+++ main/vector2D.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <math.h>
+#include <Arduino.h>
+
+class Vector2D {
+private:
+public:
+  double x, y;
+  Vector2D(double xval = 0, double yval = 0);
+  Vector2D operator+(Vector2D const& obj) const;
+  Vector2D operator*(double const& d) const;    //scalar multiplication
+  double operator*(Vector2D const& obj) const;  //dot product
+  void print() const;
+  double mag() const;
+  Vector2D normalized() const;
+  Vector2D proj(const Vector2D& v2D) const;
+};
